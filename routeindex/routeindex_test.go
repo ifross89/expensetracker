4280ca0640e0942ac6bf7f9be986f6315f1bf339
Ian Ross <ian.ross@citrix.com>
2014-12-05 16:11:33 +0000

    Added more tests. Coverage is now 100 percent although more tests could still be added!

--- routeindex/routeindex_test.go
+++ routeindex/routeindex_test.go
@@ -1,27 +1,28 @@
 package routeindex
 
 import (
-	"testing"
 	. "github.com/smartystreets/goconvey/convey"
+	"testing"
 )
+
+var index = CreateMemoryIndex(
+	[]RouteInfo{
+		RouteInfo{
+			RouteString: "/testroute1",
+			Name:        "test1",
+		},
+		RouteInfo{
+			RouteString: "/testroute2/:category/:id",
+			Name:        "test2",
+		},
+		RouteInfo{
+			RouteString: "/testroute3/:a",
+			Name:        "test3",
+		},
+	}...)
 
 func TestRetrieveRoute(t *testing.T) {
 	Convey("Given an index with some starting values", t, func() {
-		index := CreateMemoryIndex(
-		[]RouteInfo{
-			RouteInfo{
-				RouteString:  "/testroute1",
-				Name: "test1",
-			},
-			RouteInfo{
-				RouteString: "/testroute2/:category/:id",
-				Name: "test2",
-			},
-			RouteInfo{
-				RouteString: "/testroute3/:a",
-				Name: "test3",
-			},
-		}...)
 
 		Convey("When attempting to retrieve a route with the same name as one added", func() {
 			r := index.MustByName("test1")
@@ -36,35 +37,46 @@ func TestRetrieveRoute(t *testing.T) {
 				So(err, ShouldNotEqual, nil)
 			})
 		})
+
+		Convey("When attempting to retrieve a route not present in the index with MustByName", func() {
+			Convey("The index should panic", func() {
+				So(func() { index.MustByName("NotPresent") }, ShouldPanic)
+			})
+		})
+	})
+}
+
+func TestURLCreationFailures(t *testing.T) {
+	Convey("Given an index initialized with various routes", t, func() {
+		Convey("The index should return an error when an invalid route name is supplied", func() {
+			_, err := index.URL("NotPresent")
+			So(err, ShouldNotEqual, nil)
+		})
+
+		Convey("The index should return an error when the arguments supplied are not in pairs", func() {
+			_, err := index.URL("test1", "oddParam")
+			So(err, ShouldNotEqual, nil)
+		})
+
+		Convey("The index should return an error with invalid parameters", func() {
+			_, err := index.URL("test2", "not_present", "wont_be_inserted")
+			So(err, ShouldNotEqual, nil)
+		})
 	})
 }
 
 func TestURLCreation(t *testing.T) {
-	index := CreateMemoryIndex(
-		[]RouteInfo{
-			RouteInfo{
-				RouteString:  "/testroute1",
-				Name: "test1",
-			},
-			RouteInfo{
-				RouteString: "/testroute2/:category/:id",
-				Name: "test2",
-			},
-			RouteInfo{
-				RouteString: "/testroute3/:a",
-				Name: "test3",
-			},
-		}...)
 
-	tests := []struct{
-			route string
-			params []string
-			shouldError bool
-			expected string}{
-				{route:"test1",params: []string{},shouldError: false, expected: "/testroute1"},
-				{route: "test2", params: []string{"category", "baseball", "id", "23"}, shouldError: false, expected: "/testroute2/baseball/23"},
-				{route: "test3", params: []string{"a", "b"}, shouldError: false, expected: "/testroute3/b"},
-			}
+	tests := []struct {
+		route       string
+		params      []string
+		shouldError bool
+		expected    string
+	}{
+		{route: "test1", params: []string{}, shouldError: false, expected: "/testroute1"},
+		{route: "test2", params: []string{"category", "baseball", "id", "23"}, shouldError: false, expected: "/testroute2/baseball/23"},
+		{route: "test3", params: []string{"a", "b"}, shouldError: false, expected: "/testroute3/b"},
+	}
 
 	for i, test := range tests {
 		url, err := index.URL(test.route, test.params...)
